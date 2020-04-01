3a1f8ea6b32a6c07ccd06a4bc1a873ec8011689f
Administrator <784748497@qq.com>
2020-04-01 16:35:07 +0800

    4-1

--- app/src/main/java/com/example/aydownloader/net/HttpManager.java
+++ app/src/main/java/com/example/aydownloader/net/HttpManager.java
@@ -1,6 +1,102 @@
 package com.example.aydownloader.net;
 
+import okhttp3.Call;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.util.concurrent.TimeUnit;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class HttpManager {
+
+    private OkHttpClient.Builder builder;
+
+    private HttpManager(){
+        builder = new OkHttpClient.Builder()
+        .connectTimeout(10, TimeUnit.SECONDS)
+        .writeTimeout(10, TimeUnit.SECONDS)
+        .readTimeout(10, TimeUnit.SECONDS);
+    }
+
+    public static HttpManager getInstance(){
+        return HttpManagerHolder.instance;
+    }
+
+    private static class HttpManagerHolder{
+        private static final HttpManager instance = new HttpManager();
+    }
+
+    public Call initRequest(String url, long start, long end, final Callback callback){
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Range", "bytes=" + start + "-" + end)
+                .build();
+        Call call = builder.build().newCall(request);
+        call.enqueue(callback);
+        return call;
+    }
+
+
+    public Call initRequest(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Range", "bytes=")
+                .build();
+        Call call = builder.build().newCall(request);
+        call.execute();
+        return call;
+    }
+
+    public Response initRequest(String url, String lastModify) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .header("Range", "bytes=0-")
+                .header("If-Range", lastModify)
+                .build();
+
+        return builder.build().newCall(request).execute();
+    }
+
+    public void setCertificates(InputStream... certificates){
+        try {
+            CertificateFactory factory = CertificateFactory.getInstance("X.509");
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(null);
+            int index = 0;
+            for (InputStream c : certificates){
+                String certificateAlias = Integer.toString(index++);
+                keyStore.setCertificateEntry(certificateAlias, factory.generateCertificate(c));
+                if (c != null) c.close();
+            }
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            TrustManagerFactory managerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            managerFactory.init(keyStore);
+            sslContext.init(null, managerFactory.getTrustManagers(), new SecureRandom());
+            builder.sslSocketFactory(sslContext.getSocketFactory());
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
